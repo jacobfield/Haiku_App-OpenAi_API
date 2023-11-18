95283c00ba54f84f36a5f5c04aab49e6bc56521d
jacobfield <jacobprogrammes@gmail.com>
2023-11-18 15:33:10 +0000

    typewriter working, but also rendering an undefined character index. About to attempt to remove the .undefined

--- haikus/src/components/Haikus.js
+++ haikus/src/components/Haikus.js
@@ -4,28 +4,50 @@ import { useState, useEffect } from "react";
 import { fetchHaiku } from "./fetchHaiku.js";
 
 export function Haikus({ haikuData }) {
+  // declaring state to be utilised
   const [inputValue, setInputValue] = useState("");
   const [generatedHaiku, setGeneratedHaiku] = useState("");
+  const [typewriterText, setTypewriterText] = useState("");
+  //
   // function to handle input change
   function handleInputChange(event) {
     setInputValue(event.target.value);
   }
+  //
   // function to handle form submission
   async function handleSubmit(event) {
     event.preventDefault();
     let inputTopic = inputValue;
-    // setGeneratedHaiku(newHaiku);
-    // setGeneratedHaiku(fetchHaiku(inputTopic).gtpResponse);
     let newHaiku = await fetchHaiku(inputTopic);
     setGeneratedHaiku(newHaiku);
     console.log(generatedHaiku);
+    setTypewriterText("");
   }
+  //
+  // typewriter effect
+  useEffect(() => {
+    let index = -1;
+    const typingInterval = setInterval(() => {
+      setTypewriterText(
+        (prevtypewriterText) => prevtypewriterText + generatedHaiku[index]
+      );
+      index++;
+
+      if (index === generatedHaiku.length) {
+        clearInterval(typingInterval);
+      }
+    }, 75); // Adjust the timing as needed
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(typingInterval);
+  }, [generatedHaiku]);
+
   const defaultText = "On which topic do you seek ancient wisdom?";
   return (
     <section className="haikusAll">
       <div className="haikuDiv">
         <p className="displayText">
-          {!generatedHaiku ? defaultText : generatedHaiku}
+          {!generatedHaiku ? defaultText : typewriterText}
         </p>
       </div>
       <form className="inputTopic" onSubmit={handleSubmit}>
